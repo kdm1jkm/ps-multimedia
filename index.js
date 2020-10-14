66612671f323d170c09ea27f06b7b42bd63f78d8
kdm1jkm <kdm1jkmm@gmail.com>
2020-10-15 07:57:15 +0900

    Add apply form
    Added form in index.html and handleSubmit() in index.js

--- index.js
+++ index.js
@@ -1,26 +1,90 @@
-const applyForm = document.querySelector(".js-apply");
-const applyList = applyForm.querySelector("ul");
+const applyInfo = document.querySelector(".js-apply");
+const applyInfoList = applyInfo.querySelector("ul");
+const applyForm = document.querySelector(".js-form");
+const applyForms = {
+  grade: document.querySelector(".form__grade"),
+  class_num: document.querySelector(".form__class"),
+  number: document.querySelector(".form__number"),
+  period: document.querySelector(".form__period"),
+};
 
-const API_URL = "http://127.0.0.1:5000/";
+function isLocal() {
+  return (
+    window.location.href === "127.0.0.1" || window.location.href === "localhost"
+  );
+}
 
-function init() {
-  const span = document.createElement("span");
-  applyForm.appendChild(span);
+const API_URLS = {
+  get: isLocal() ? "http://127.0.0.1:5000/" : "",
+  apply: isLocal() ? "http://127.0.0.1:5000/apply" : "",
+};
 
+function clearInput() {
+  applyForms.grade.value = "";
+  applyForms.class_num.value = "";
+  applyForms.number.value = "";
+  applyForms.period.value = "";
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const { grade, class_num, number } = applyForms;
+
+  const student_number = `${grade.value}${class_num.value}${
+    number.value > 9 ? `${number.value}` : `0${number.value}`
+  }`;
+
+  const period = applyForms.period.value;
+
+  const data = {
+    student_number,
+    period,
+  };
+
+  fetch(API_URLS.apply, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      loadData();
+    })
+    .then(() => {
+      clearInput();
+    });
+}
+
+function clearData() {
+  applyInfoList.innerHTML = "";
+}
+
+function loadData() {
+  clearData();
   let text;
-  fetch(API_URL)
+  fetch(API_URLS.get)
     .then((response) => {
       return response.json();
     })
     .then((json) => {
-      json.forEach((period) => {
-        const ol = document.createElement("ol");
+      json.forEach((period, i) => {
         const li = document.createElement("li");
-        applyList.appendChild(li);
+        const h1 = document.createElement("h1");
+        const ol = document.createElement("ol");
+
+        applyInfoList.appendChild(li);
+
+        li.appendChild(h1);
         li.appendChild(ol);
+
+        h1.innerText = `${i + 1}교시`;
+
         period.forEach((student) => {
-          const p = document.createElement("p");
           const li = document.createElement("li");
+          const p = document.createElement("p");
+
           ol.appendChild(li);
           li.appendChild(p);
 
@@ -28,6 +92,11 @@ function init() {
         });
       });
     });
+}
+
+function init() {
+  loadData();
+  applyForm.addEventListener("submit", handleSubmit);
 }
 
 init();
